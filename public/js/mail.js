5e4f13c066e43bea89ab400f1c3fdcf4d340ced9
Baby Nap Boltron <147931096+babynapboltron8@users.noreply.github.com>
2025-02-09 12:46:03 +0800

    testing contact form

--- public/js/mail.js
+++ public/js/mail.js
@@ -1,9 +1,34 @@
 // ! NEEDS TO BE UPDATED TO WORK WITH NODEMAILER
 const contactForm = document.querySelectorAll('.form-1, .form-2');
+let fname = document.getElementById('fname');
+let email = document.getElementById('email');
+let message = document.getElementById('message');
 
 contactForm.forEach((form) => {
   form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log('Form submitted');
+
+    let formData = {
+      fname: fname.value,
+      email: email.value,
+      message: message.value,
+    };
+    console.log(formData);
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', '/send');
+    xhr.setRequestHeader('content-type', 'application/json');
+    xhr.onload = function () {
+      console.log(xhr.responseText);
+      if (xhr.responseText == 'success') {
+        alert('Email sent');
+        fname.value = '';
+        email.value = '';
+        message.value = '';
+      } else {
+        alert('Something went wrong!');
+      }
+    };
+    xhr.send(JSON.stringify(formData));
   });
 });
--- server.js
+++ server.js
@@ -1,15 +1,45 @@
 const express = require('express');
 const app = express();
 
+const nodemailer = require('nodemailer');
+
 const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.static('public'));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.post('/send', (req, res) => {
+  console.log(req.body);
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'oYV0v@example.com',
+      pass: 'password',
+    },
+  });
+
+  const mailOptions = {
+    from: req.body.email,
+    to: 'napboltron@outlook.com',
+    text: `Name: ${req.body.fname}\nEmail: ${req.body.email}\nMessage: ${req.body.message}`,
+  };
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send('error');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.send('success');
+    }
+  });
 });
